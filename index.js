4bba84b0159a642e85cad0ffe8ef2ce8a3ff1e7d
JannatulHappy <jannatulferdoushappy21@gmail.com>
2021-11-13 01:23:27 +0600

    dashboard-server-side-updated

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectId;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kkrwn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-console.log(uri)
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+console.log(uri);
 const port = 5000;
 const app = express();
 app.use(cors());
@@ -22,27 +25,138 @@ client.connect((err) => {
   const servicesCollection = client.db("karBox").collection("collections");
   const usersCollection = client.db("karBox").collection("users");
   const ordersCollection = client.db("karBox").collection("orders");
-  const reviewCollection = client.db("karBox").collection("review");
+  const reviewsCollection = client.db("karBox").collection("reviews");
 
+  console.log("database connected");
+  // get all collections
 
-console.log("database connected")
- // get all collections
-
- app.get("/allCollections", async (req, res) => {
+  app.get("/allCollections", async (req, res) => {
     const result = await servicesCollection.find({}).toArray();
     res.send(result);
   });
-// get single  collection
+  // get single  collection
 
-app.get("/singleCollection/:id",async (req, res) => {
-    console.log(req.params.id);
+  app.get("/singleCollection/:id", async (req, res) => {
     const result = await servicesCollection
       .find({ _id: ObjectId(req.params.id) })
       .toArray();
     res.send(result[0]);
+  });
+  // add booking
+
+  app.post("/addBooking", async (req, res) => {
+    console.log(req.body);
+    const result = await ordersCollection.insertOne(req.body);
+    res.send(result);
+  });
+
+  // make admin
+  app.put("/users/admin", async (req, res) => {
+    const user = req.body;
+    const filter = { email: user.email };
+    const updateDoc = { $set: { role: "admin" } };
+    const result = await usersCollection.updateOne(filter, updateDoc);
+    console.log(result);
+    res.json(result);
+  });
+
+  // check  admin or not
+  app.get("/users/:email", async (req, res) => {
+    const email = req.params.email;
+    const query = { email: email };
+    const user = await usersCollection.findOne(query);
+    let isAdmin = false;
+    if (user?.role === "admin") {
+      isAdmin = true;
+    }
+    res.json({ admin: isAdmin });
+  });
+  // add user
+
+  app.post("/users", async (req, res) => {
+    const user = req.body;
+    const result = await usersCollection.insertOne(user);
+    console.log(result);
+    res.json(result);
+  });
+  // add user update
+  app.put("/users", async (req, res) => {
+    const user = req.body;
+    const filter = { email: user.email };
+    const options = { upsert: true };
+    const updateDoc = { $set: user };
+    const result = await usersCollection.updateOne(filter, updateDoc, options);
+    res.json(result);
+  });
+  // get reviews
+  app.get("/reviews", async (req, res) => {
+    const result = await reviewsCollection.find({}).toArray();
+    res.send(result);
+  });
+  //add  review
+  app.post("/addReview", async (req, res) => {
+    const result = await reviewsCollection.insertOne(req.body);
+
+    res.send(result);
+  });
+  //add Collection
+  app.post("/addCollection", async (req, res) => {
+    const result = await servicesCollection.insertOne(req.body);
+    console.log(result);
+    res.send(result);
+  });
+  //get  my order by using email query
+
+  app.get("/myOrders/:email", async (req, res) => {
+    const result = await ordersCollection
+      .find({
+        userEmail: req.params.email,
+      })
+      .toArray();
+    res.send(result);
+  });
+
+  // delete an order from my order
+
+  app.delete("/deleteOrder/:id", async (req, res) => {
+    const result = await ordersCollection.deleteOne({
+      _id: ObjectId(req.params.id),
+    });
+    res.send(result);
+  });
+   /// all order
+   app.get("/allOrders", async (req, res) => {
+    // console.log("hello");
+    const result = await ordersCollection.find({}).toArray();
+    res.send(result);
+  });
+  // status update
+  // status update
+  app.put("/statusUpdate/:id", async (req, res) => {
+    const filter = { _id: ObjectId(req.params.id) };
+    console.log(req.params.id);
+    const result = await ordersCollection.updateOne(filter, {
+      $set: {
+        status: req.body.status,
+      },
+    });
+    res.send(result);
     console.log(result);
   });
- 
+   // delete booking from manage booking
+   app.delete("/DeleteManageBooking/:id", async (req, res) => {
+    const result = await ordersCollection.deleteOne({
+      _id: ObjectId(req.params.id),
+    });
+    res.send(result);
+  });
+   // delete collections from manage collection
+   app.delete("/deleteManageCollection/:id", async (req, res) => {
+    const result = await servicesCollection.deleteOne({
+      _id: ObjectId(req.params.id),
+    });
+    res.send(result);
+  });
 });
 
 app.listen(process.env.PORT || port);
